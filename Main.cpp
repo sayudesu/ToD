14bdf6be6c89e0707bb06a4b396bbb3e898e3b08
sayuGitHub <114457066+sayudesu@users.noreply.github.com>
2023-09-15 09:04:37 +0900

    modosu

--- Main.cpp
+++ Main.cpp
@@ -134,34 +134,9 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
 		// 画面のクリア
 		ClearDrawScreen();
 
-		DINPUT_JOYSTATE input;
-		// 入力状態を取得
-		GetJoypadDirectInputState(DX_INPUT_PAD1, &input);
-
-		// 画面に構造体の中身を描画
-		int Color = GetColor(255, 255, 255);
-		DrawFormatString(0, 0, Color, "X:%d Y:%d Z:%d",
-			input.X, input.Y, input.Z);
-		DrawFormatString(0, 16, Color, "Rx:%d Ry:%d Rz:%d",
-			input.Rx, input.Ry, input.Rz);
-		DrawFormatString(0, 32, Color, "Slider 0:%d 1:%d",
-			input.Slider[0], input.Slider[1]);
-		DrawFormatString(0, 48, Color, "POV 0:%d 1:%d 2:%d 3:%d",
-			input.POV[0], input.POV[1],
-			input.POV[2], input.POV[3]);
-		DrawString(0, 64, "Button", Color);
-		for (int i = 0; i < 32; i++)
-		{
-			DrawFormatString(64 + i % 8 * 64, 64 + i / 8 * 16, Color,
-				"%2d:%d", i, input.Buttons[i]);
-		}
-
-
-
-
 		pScene->Update();
 		//SetDrawScreen(m_hScreen);
-		//pScene->Draw();
+		pScene->Draw();
 		if (DxLib::CheckHitKey(KEY_INPUT_1))
 		{
 			level -= 0.3f;
